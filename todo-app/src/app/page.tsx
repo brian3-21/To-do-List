e817c1d7debfa48ee50be897538b64f504289950
Brian3-21 <brianperezdota@gmail.com>
2025-06-11 12:42:37 -0400

    init

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,47 +1,35 @@
 'use client';
+import { useState } from 'react';
+import { Button } from '../components/ui/Button';
 
 export default function Home() {
 
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
 
   function incrementCounter(){
-     counter++;
-     console.log("el contador es: ",counter)
+    setCounter(counter + 1);
+    console.log("el contador es: ",counter)
+  }
+  function decrementCounter(){
+    setCounter(counter - 1);
+    console.log("el contador es: ",counter)
   }
 
   return (
       <div className="h-screen flex flex-col items-center justify-center text-gray-900 dark:text-white">
         <h1 className="text-[12rem] leading-none">Hola Tanque</h1> {/* leading-none elimina espacio entre líneas */}
         <p className="text-[6rem]">El contador es: {counter}</p>
-      <button 
-        onClick={incrementCounter}
-        type="button" 
-        className="
-          text-4xl                      /* Texto XL */
-          w-16 h-16                     /* Tamaño grande */
-          ml-4                          /* Margen izquierdo */
-          text-blue-600                 /* Color azul claro */
-          hover:bg-blue-600             /* Hover azul */
-          hover:text-white              /* Texto blanco en hover */
-          dark:text-blue-400            /* Azul claro en oscuro */
-          dark:hover:bg-blue-500        /* Hover azul oscuro */
-          dark:hover:text-white         /* Texto blanco en oscuro */
-          border-2                      /* Borde más grueso */
-          border-blue-600               /* Borde azul */
-          dark:border-blue-400          /* Borde azul oscuro */
-          rounded-full                  /* Forma circular */
-          flex                          /* Flex para centrar */
-          items-center                  /* Centrado vertical */
-          justify-center                /* Centrado horizontal */
-          transition-colors             /* Transición suave */
-          duration-300                  /* Duración transición */
-          focus:outline-none            /* Elimina outline */
-          focus:ring-4                  /* Anillo de enfoque */
-          focus:ring-blue-300           /* Color anillo */
-          dark:focus:ring-blue-800      /* Anillo oscuro */
-        ">
+        <form className="max-w-sm mx-auto">
+          <label htmlFor="number-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a number:</label>
+          <input onSubmit={ ev =>{ev.preventDefault();console.log(ev.currentTarget.value)}} type="number" id="number-input" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xxxxxxx" required />
+        </form  >
+        <Button variant="danger" size="md" onClick={incrementCounter} >
         +
-      </button>
+        </Button>
+        <Button variant="danger" size="md" onClick={decrementCounter} >
+        -
+        </Button>
       </div>
+      
   );
 }
